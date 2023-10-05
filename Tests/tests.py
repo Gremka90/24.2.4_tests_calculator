4f15ffab86a508613312d631c86c6c538919d9e4
Вячеслав <gremka90@gmail.com>
2023-10-05 23:25:46 +0300

    Completed tasks

---
+++ Tests/tests.py
@@ -0,0 +1,21 @@
+from app import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 1, 1) == 2
+
+    def test_subtraction(self):
+        assert self.calc.subtraction(self, 3, 1) == 2
+
+    def test_division(self):
+        assert self.calc.division(self, 20, 5) == 4
+
+    def test_multiply(self):
+        assert self.calc.multiply(self, 4, 5) == 20
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
